010f03b37ad89714a944999ed11ca40a2b836314
Jesus Palencia <palencia77@gmail.com>
2018-05-27 20:54:38 -0300

    changes in DB config

--- com/__init__.py
+++ com/__init__.py
@@ -10,7 +10,8 @@ print "**************PATH_ROOT: " + PATH_ROOT
 #    PATH_ROOT += "/"
 
 db_config = {}
-execfile(PATH_ROOT+"/conf/properties/mongodb.cfg", db_config)
+#execfile(PATH_ROOT+"/conf/properties/mongodb.cfg", db_config)
+execfile("conf/properties/mongodb.cfg", db_config)
 services_app.config['MONGODB_SETTINGS'] = {
                                             'DB': db_config["DB"],
                                             'HOST': db_config["HOST"],
@@ -18,7 +19,7 @@ services_app.config['MONGODB_SETTINGS'] = {
                                             'USERNAME': db_config["USERNAME"],
                                             'PASSWORD': db_config["PASSWORD"]
                                            }
-services_app.jinja_loader = jinja2.FileSystemLoader(PATH_ROOT+"/conf/templates")
+services_app.jinja_loader = jinja2.FileSystemLoader("conf/templates")
 db = MongoEngine(services_app)
 if __name__ == '__main__':
     services_app.run('0.0.0.0', 5000, True)
